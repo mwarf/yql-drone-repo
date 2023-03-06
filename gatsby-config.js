5bac4f74e12babe6feb0c07515d0b7c994ef375c
Michael Warf <michael@coalbanks.com>
2023-03-06 04:56:55 -0700

    updating local studio plugin to refresh on change

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,8 @@ module.exports = {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_PROJECT_DATASET,
         token: process.env.SANITY_READ_TOKEN,
+        watchMode: true,
+        overlayDrafts: true,
       },
     },
     "gatsby-plugin-sharp",
